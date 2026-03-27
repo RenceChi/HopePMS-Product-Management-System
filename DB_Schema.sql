9c31701c80a421fa4b0ec8b4c4b0d3fb16793f02
rramayuga <58220050+rramayuga@users.noreply.github.com>
2026-03-27 19:27:20 +0800

    PR-01 db/initial-schema

--- DB_Schema.sql
+++ DB_Schema.sql
@@ -39,11 +39,7 @@ CREATE TABLE public.jobhistory (
   CONSTRAINT jobhistory_jobcode_fkey FOREIGN KEY (jobcode) REFERENCES public.job(jobcode),
   CONSTRAINT jobhistory_deptcode_fkey FOREIGN KEY (deptcode) REFERENCES public.department(deptcode)
 );
-CREATE TABLE public.module (
-  module_id character varying NOT NULL,
-  module_name character varying,
-  CONSTRAINT module_pkey PRIMARY KEY (module_id)
-);
+
 CREATE TABLE public.payment (
   orno character varying NOT NULL,
   paydate date,
@@ -74,11 +70,6 @@ CREATE TABLE public.product (
   updated_by character varying,
   CONSTRAINT product_pkey PRIMARY KEY (prodcode)
 );
-CREATE TABLE public.rights (
-  right_id character varying NOT NULL,
-  right_name character varying,
-  CONSTRAINT rights_pkey PRIMARY KEY (right_id)
-);
 CREATE TABLE public.sales (
   transno character varying NOT NULL,
   salesdate date,
@@ -96,42 +87,8 @@ CREATE TABLE public.salesdetail (
   CONSTRAINT salesdetail_transno_fkey FOREIGN KEY (transno) REFERENCES public.sales(transno),
   CONSTRAINT salesdetail_prodcode_fkey FOREIGN KEY (prodcode) REFERENCES public.product(prodcode)
 );
-CREATE TABLE public.user (
-  userid character varying NOT NULL,
-  email character varying NOT NULL UNIQUE,
-  username character varying,
-  user_type character varying DEFAULT 'USER'::character varying CHECK (user_type::text = ANY (ARRAY['SUPERADMIN'::character varying, 'ADMIN'::character varying, 'USER'::character varying]::text[])),
-  record_status character varying DEFAULT 'ACTIVE'::character varying,
-  created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-  created_by character varying,
-  updated_at timestamp without time zone,
-  updated_by character varying,
-  CONSTRAINT user_pkey PRIMARY KEY (userid)
-);
-CREATE TABLE public.user_module (
-  userid character varying NOT NULL,
-  module_id character varying NOT NULL,
-  rights_value integer DEFAULT 0,
-  record_status character varying DEFAULT 'ACTIVE'::character varying,
-  created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-  created_by character varying,
-  updated_at timestamp without time zone,
-  updated_by character varying,
-  CONSTRAINT user_module_pkey PRIMARY KEY (userid, module_id),
-  CONSTRAINT user_module_module_id_fkey FOREIGN KEY (module_id) REFERENCES public.module(module_id)
-);
-CREATE TABLE public.user_module_rights (
-  userid character varying NOT NULL,
-  module_id character varying NOT NULL,
+CREATE TABLE public.rights (
   right_id character varying NOT NULL,
-  rights_value integer DEFAULT 0,
-  record_status character varying DEFAULT 'ACTIVE'::character varying,
-  created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-  created_by character varying,
-  updated_at timestamp without time zone,
-  updated_by character varying,
-  CONSTRAINT user_module_rights_pkey PRIMARY KEY (userid, module_id, right_id),
-  CONSTRAINT user_module_rights_userid_fkey FOREIGN KEY (userid) REFERENCES public.user(userid),
-  CONSTRAINT user_module_rights_module_id_fkey FOREIGN KEY (module_id) REFERENCES public.module(module_id),
-  CONSTRAINT user_module_rights_right_id_fkey FOREIGN KEY (right_id) REFERENCES public.rights(right_id)
+  right_name character varying,
+  CONSTRAINT rights_pkey PRIMARY KEY (right_id)
 );
